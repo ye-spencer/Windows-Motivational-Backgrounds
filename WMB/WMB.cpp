434f5873d1b474890a640fe432ada1a65ec0c980
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2024-08-02 09:58:24 +0800

    read through a directory successfully

--- WMB/WMB.cpp
+++ WMB/WMB.cpp
@@ -1,12 +1,38 @@
+/* WMB.cpp
+ * Author: Spencer Ye
+ * Last Edited: August 2nd, 2024
+ * Version: 0.3.0
+ */
+
 #include <windows.h>
 #include <iostream>
+#include <filesystem>
+#include <string>
 
 bool setWallpaper(std::string imageName);
+
+namespace fs = std::filesystem;
 
 int main()
 {
     std::cout << "HELLO" << std::endl;
     setWallpaper("JHU_Admission.png");
+
+    // Path to the directory
+    std::string path = "C:\\Users\\spenc\\Documents\\GitHub\\Windows-Motivational-Backgrounds\\WMB\\images";
+
+    size_t size = 0;
+
+    // Looping until all the items of the directory are exhausted
+    for (const auto& entry : fs::directory_iterator(path)) {
+
+        // Converting the path to a string
+        std::string str = entry.path().string();
+
+        std::cout << str << "DIFF" << std::endl;
+        size++;
+
+    }
 }
 
 bool setWallpaper(std::string imageName)
