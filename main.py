c3a1f7d936a6af4dae2d178f661e41a6b3627906
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2024-08-02 11:39:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,47 @@
 # Last Modified: August 2nd, 2024
 # Version: 0.3.0
 
-#from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont
 import pathlib
-import sys
-print(sys.executable)
-exit(0)
-
-# Load the image
-curr_directory = str(pathlib.Path(__file__).parent.resolve())
+import pyautogui
 
 
-image = Image.open(curr_directory + "\\background_images\\background.jpg")
+# BEGIN CONSTANTS
 
-# Create a drawing context
-draw = ImageDraw.Draw(image)
+BACKGROUND_COLOR = (210, 216, 216)
 
-# Define the text properties
-font = ImageFont.truetype(curr_directory + "\\fonts\\OldNewspaperTypes.ttf", 36)
-text = "Hello, World!"
-position = (50, 50)
+# END CONSTANTS
 
-# Add text to the image
-draw.text(position, text, font=font)
 
-# Save or display the modified image
-image.save(str(curr_directory) + "\\outs.jpg")
+def generate_image(text : str):
+# Load the current directory
+    curr_directory = str(pathlib.Path(__file__).parent.resolve())
+
+    # Get the current device's screen resolutions
+    W, H = pyautogui.size()
+
+    # Create background image
+    image = Image.new(mode = "RGB", size = (W, H), color = BACKGROUND_COLOR)
+
+    # Create a drawing context
+    draw = ImageDraw.Draw(image)
+
+    # Define the font
+    font = ImageFont.truetype(curr_directory + "\\fonts\\OldNewspaperTypes.ttf", 36)
+
+    # Get the dimensions of the text so we can center it
+    _, _, w, h = draw.textbbox((0, 0), text=text, font=font)
+
+    # Add text to the image
+    draw.text(((W - w)/2, (H - h)/2.25), text, font=font, fill="black")
+
+    # Save the image in our directory
+    image.save(str(curr_directory) + "\\WMB\\images\\outst.jpg")
+
+def main():
+    # TODO: take in text as an argument
+    text = "Hello, World!"
+    generate_image(text)
+
+if __name__ == "__main__":
+    main()
